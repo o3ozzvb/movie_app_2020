63dced238b1cefd2ef004d131dfa09d13eee6199
oeunkyoung <ou1112@naver.com>
2020-06-07 16:27:41 +0900

    최종 완성 코드

---
+++ src/routes/Home.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import axios from 'axios';
+import Movie from '../components/Movie';
+import './Home.css';
+
+class Home extends React.Component {
+  state = {
+    isLoading: true,
+  };
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+     } = await axios.get("http://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+     //console.log(movies);
+     this.setState({ movies, isLoading: false }); //this.setState({movies:movies});
+  }
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  componentDidUpdate() {
+    console.log('I just updated');
+  }
+  
+  componentWillUnmount() {
+    console.log('Goodbye, cruel world');
+  }
+
+  render() {
+    const {isLoading, movies} = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie 
+               key={movie.id}
+               id={movie.id}
+               year={movie.year}
+               title={movie.title}
+               summary={movie.summary}
+               poster={movie.medium_cover_image}
+               genres={movie.genres}
+              />
+          ))}
+          </div>
+        )}
+        </section>
+    );
+  }
+}
+
+export default Home;                                                                     
