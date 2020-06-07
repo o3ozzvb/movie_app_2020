971c0ff49b5ea45e142b678c1b18ffdb5d64bca0
oeunkyoung <ou1112@naver.com>
2020-06-07 16:59:44 +0900

    change home.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -13,7 +13,7 @@ class Home extends React.Component {
       data: {
         data: { movies },
       },
-     } = await axios.get("http://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+     } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
      //console.log(movies);
      this.setState({ movies, isLoading: false }); //this.setState({movies:movies});
   }
